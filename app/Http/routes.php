b949531bd373ce9c54063098bc3316e15fe35ac1
takabo <thousandbeach.td@gmail.com>
2018-02-27 22:05:55 +0000

    CRUD messages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,46 @@
 |
 */
 
+/*  デフォルトのもの これを下記のもののように書き換える
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+// Route::get('/', 'MessagesController@index');
+
+
+/*
+// CRUD
+// メッセージの個別詳細ページ表示
+Route::get('messages/{id}', 'MessagesController@show');
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
+Route::post('messages', 'MessagesController@store');
+// メッセージの更新処理（編集画面を表示するためのものではありません）
+Route::put('messages/{id}', 'MessagesController@update');
+// メッセージを削除
+Route::delete('messages/{id}', 'MessagesController@destroy');
+
+
+// index:  showの補助ページ
+// 詳細ページ（show）にアクセスするためには、一覧ページ（index）が必要。
+Route::get('messages', 'MessagesController@index');
+
+// create:  新規作成用のフォームページ
+// 保存アクション（store）にデータを送るには、新規作成用のフォームページ（create）が必要。
+Route::get('messages/create', 'MessagesController@create');
+
+// edit:  更新用のフォームページ
+// 更新ページ（edit）にデータを送るには、更新用のフォームページ（edit）が必要。
+Route::put('message/{id}/edit', 'MessagesController@edit');
+
+
+
+Route::resource('messages', 'MessagesController');
+// copy f複写機能
+Route::get('messages/{id}/copy', 'MessagesController@copy');
+
+*/
+
+Route::get('/', 'MessagesController@index');
+Route::resource('messages', 'MessagesController');
---
+++ resources/views/messages/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ新規作成ページ</h1>
+    
+    {!! Form::model($message, ['route' => 'messages.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+        
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+@endsection
+
+{{ '<p style="color: red;">htmlentities 関数に通した場合</p>' }}
+{!! '<p style="color: red;">htmlentities 関数に通さなかった場合</p>' !!}
